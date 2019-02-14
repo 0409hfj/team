ada56e92553053687b4e2050db1bbdc014b23a67
wangfang <2404956029@qq.com>
2019-02-14 20:51:17 +0800

    导航

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var webserver = require('gulp-webserver');
+gulp.task('sass', function() {
+    return gulp.src('./src/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./src/css'))
+})
+gulp.task('watch', function() {
+    return gulp.watch('./src/scss/*.scss', gulp.series('sass'))
+})
+gulp.task('webserver', function() {
+    return gulp.src('src')
+        .pipe(webserver({
+            port: 8088,
+            open: true,
+            livereload: true
+        }))
+})
+gulp.task('dev', gulp.series('sass', 'webserver', 'watch'))
